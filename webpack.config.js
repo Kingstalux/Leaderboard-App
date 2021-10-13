ac3be9a1eabe084a2abc3de3df3ad9229e381d09
Kingstalux <kingsleyngu25@gmail.com>
2021-10-13 05:28:16 +0100

    Finish setting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
   module: {
     rules: [
