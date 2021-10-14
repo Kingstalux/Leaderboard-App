9291f244c87bfa5bd4767a103346c342273250df
Kingstalux <kingsleyngu25@gmail.com>
2021-10-14 17:13:27 +0100

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -2,68 +2,65 @@
 import './style.css';
 import displayData from './script1.js';
 
-const form = document.getElementById('form')
+const form = document.getElementById('form');
 
-async function getData () {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mTnreNhieKoISaSkVF7a/scores')
+async function getData() {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mTnreNhieKoISaSkVF7a/scores');
 
-  const json = await response.json()
+  const json = await response.json();
 
-  const container = document.getElementById('container')
-  container.innerHTML = ''
-  json.result.forEach(element => {
-    let name = element.user
-    let number = element.score
-    displayData(name, number)
+  const container = document.getElementById('container');
+  container.innerHTML = '';
+  json.result.forEach((element) => {
+    const name = element.user;
+    const number = element.score;
+    displayData(name, number);
   });
 }
 
-  getData()
+getData();
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const name = document.getElementById('name').value
-  const score = document.getElementById('number').value
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('number').value;
 
-  async function createGame () {
+  async function createGame() {
     const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
       method: 'post',
-      body: JSON.stringify({ 
-        name: "GOT character rankings" 
+      body: JSON.stringify({
+        name: 'Game Of Thrones character rankings',
       }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
-    })
-
-    const json = await response.json()
+    });
+    /* eslint-disable */
+    const json = await response.json();
   }
 
-  async function postScores () {
-    const response = await   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mTnreNhieKoISaSkVF7a/scores', {
+  async function postScores() {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mTnreNhieKoISaSkVF7a/scores', {
       method: 'post',
-      body: JSON.stringify({ 
+      body: JSON.stringify({
         user: name,
-        score: score 
+        score,
       }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
-    })
-
-    const json = await response.json()
+    });
+    /* eslint-disable */
+    const json = await response.json();
   }
 
-  document.getElementById('name').value = ''
-  document.getElementById('number').value = ''
-  
-  createGame()
-  postScores()
-  })
+  document.getElementById('name').value = '';
+  document.getElementById('number').value = '';
 
-  document.getElementById('refresh').addEventListener('click', () => {
-    getData()
-  })
+  createGame();
+  postScores();
+});
 
-
-
+document.getElementById('refresh').addEventListener('click', () => {
+  getData();
+});
