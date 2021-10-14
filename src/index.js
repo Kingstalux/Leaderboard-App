6784f9f7e1f93a462ae56bb79b0b5a4d7839450d
Kingstalux <kingsleyngu25@gmail.com>
2021-10-14 16:59:06 +0100

    Add function to get data from API

--- src/index.js
+++ src/index.js
@@ -2,4 +2,22 @@
 import './style.css';
 import displayData from './script1.js';
 
+const form = document.getElementById('form')
+
+async function getData () {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mTnreNhieKoISaSkVF7a/scores')
+
+  const json = await response.json()
+
+  const container = document.getElementById('container')
+  container.innerHTML = ''
+  json.result.forEach(element => {
+    let name = element.user
+    let number = element.score
+    displayData(name, number)
+  });
+}
+
+  getData()
+
 
