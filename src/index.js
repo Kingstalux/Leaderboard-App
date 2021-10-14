3dd2d3881a5b40e5f1f8bb3db3954eedc88889fb
Kingstalux <kingsleyngu25@gmail.com>
2021-10-14 16:59:40 +0100

    Add function to post data to API

--- src/index.js
+++ src/index.js
@@ -20,4 +20,50 @@ async function getData () {
 
   getData()
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const name = document.getElementById('name').value
+  const score = document.getElementById('number').value
+
+  async function createGame () {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+      method: 'post',
+      body: JSON.stringify({ 
+        name: "GOT character rankings" 
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+
+    const json = await response.json()
+  }
+
+  async function postScores () {
+    const response = await   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mTnreNhieKoISaSkVF7a/scores', {
+      method: 'post',
+      body: JSON.stringify({ 
+        user: name,
+        score: score 
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+
+    const json = await response.json()
+  }
+
+  document.getElementById('name').value = ''
+  document.getElementById('number').value = ''
+  
+  createGame()
+  postScores()
+  })
+
+  document.getElementById('refresh').addEventListener('click', () => {
+    getData()
+  })
+
+
 
